06a6bdc2e2b3609dd690ee434a5b2d68c4354bd1
jordanny277 <salasjorandan@gmail.com>
2026-02-28 23:42:03 -0500

    Add Mavenproject2jordan class for score calculation
    This class calculates the total score based on the number of games won, drawn, and lost.

---
+++ Mano.java
@@ -0,0 +1,23 @@
+package com.mycompany.mavenproject2jordan;
+import java.util.Scanner;
+
+/**
+ * @author ALUMNO
+ */
+public class Mavenproject2jordan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int pg, pe, pp, pt;
+        System.out.println("Ingresa el numero de partidos ganados: ");
+        pg = sc.nextInt();
+
+        System.out.println("Ingresa el numero de partidos empatados: ");
+        pe = sc.nextInt();
+
+        System.out.println("Ingresa el numero de partidos perdidos: ");
+        pp = sc.nextInt();
+
+        pt = pg * 3 + pe * 1 + pp * 0;
+        System.out.println("Puntaje Total: " + pt);
+    }
+}
